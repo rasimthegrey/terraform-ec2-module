b71521b634d2247d8c241cfafa5dd8f89464be7c
Rasim Mutlu <rasim.mutlu99@gmail.com>
2024-07-25 15:10:59 +0300

    DV-182 tf aws instance configs

--- main.tf
+++ main.tf
@@ -5,9 +5,13 @@ provider "aws" {
 module "ec2_instances" {
   source = "./ec2"
 
-  instance_count = var.instance_count
-  instance_type  = var.instance_type
-  ami_id         = var.ami_id
-  key_name       = var.key_name
-  security_groups = var.security_groups
+  instance_count            = var.instance_count
+  instance_type             = var.instance_type
+  ami_id                    = var.ami_id
+  key_name                  = var.key_name
+  user_data                 = var.user_data
+  availability_zone         = var.availability_zone
+  subnet_id                 = var.subnet_id
+  vpc_security_group_ids    = var.vpc_security_group_ids
+  associate_public_ip_address = var.associate_public_ip_address
 }
--- variables.tf
+++ variables.tf
@@ -18,7 +18,29 @@ variable "key_name" {
   type        = string
 }
 
-variable "security_groups" {
+variable "user_data" {
+  description = "User data to provide when launching the instance"
+  type        = string
+  default     = ""
+}
+
+variable "availability_zone" {
+  description = "The Availability Zone to launch the instance in"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The VPC Subnet ID to launch in"
+  type        = string
+}
+
+variable "vpc_security_group_ids" {
   description = "List of security groups to assign to the instances"
   type        = list(string)
+}
+
+variable "associate_public_ip_address" {
+  description = "Associate a public IP address with the instance"
+  type        = bool
+  default     = true
 }
